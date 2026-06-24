e69bf232c862ea3c1d609e46edddd4f00cb3b898
whitexiaoLi <3073056960@qq.com>
2026-06-24 20:19:04 +0800

    Fix: simplify MyBatisPlusConfig for 3.5.9 compatibility

--- fitness-java/fitness-common/src/main/java/com/fitness/config/MyBatisPlusConfig.java
+++ fitness-java/fitness-common/src/main/java/com/fitness/config/MyBatisPlusConfig.java
@@ -1,8 +1,6 @@
 package com.fitness.config;
 
-import com.baomidou.mybatisplus.annotation.DbType;
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
-import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,8 +9,6 @@ public class MyBatisPlusConfig {
 
     @Bean
     public MybatisPlusInterceptor mybatisPlusInterceptor() {
-        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
-        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
-        return interceptor;
+        return new MybatisPlusInterceptor();
     }
 }
